296cfda5ab4213bcb2ff9841aab12628222d4e3d
Denis <ddujota@internationalsafety.com>
2018-07-02 17:22:13 -0400

    setup ui variables and crystals object that will generate a random value whenever called

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Wait for the DOM to load
+$(document).ready(function() {
+  //Game Variales
+  let CurrentGuess = 0;
+  let NumToMatch = randomNumGenerator();
+
+  // UI Variables
+  let wins = 0;
+  let losses = 0;
+  let crystals;
+
+  // Generate our initial crystals to play with
+  generateCrystals = () => {
+    return {
+      red: {
+        randomValue: Math.floor(Math.random() * 12) + 1,
+        imageUrl: 'assets/images/red.png'
+      },
+      blue: {
+        randomValue: Math.floor(Math.random() * 12) + 1,
+        imageUrl: 'assets/images/blue.png'
+      },
+      yellow: {
+        randomValue: Math.floor(Math.random() * 12) + 1,
+        imageUrl: 'assets/images/yellow.png'
+      },
+      green: {
+        randomValue: Math.floor(Math.random() * 12) + 1,
+        imageUrl: 'assets/images/green.png'
+      }
+    };
+  };
+
+  randomNumGenerator = () => {
+    return Math.floor(Math.random() * 102) + 19;
+  };
+});
